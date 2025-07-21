2042d1e88d2e58fb9f721c9b2794a76b67d6bbf4
AlyssaKhay <aelis.khay@gmail.com>
2025-07-21 11:58:16 +0700

    fixed Makefile

--- 8_dynamic_libraries/calc.h
+++ 8_dynamic_libraries/calc.h
@@ -3,16 +3,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// объяв видимости для C и C++
-#if defined(__cplusplus)
-#define EXPORT extern "C" __attribute__ ((visibility ("default")))
-#else
-#define EXPORT __attribute__ ((visibility ("default")))
-#endif
-
-EXPORT void add (int a, int b);
-EXPORT void sub (int a, int b);
-EXPORT void mul (int a, int b);
-EXPORT void divi (int a, int b);
+__attribute__ ((visibility ("default"))) void add (int a, int b);
+__attribute__ ((visibility ("default"))) void sub (int a, int b);
+__attribute__ ((visibility ("default"))) void mul (int a, int b);
+__attribute__ ((visibility ("default"))) void divi (int a, int b);
 
 #endif
